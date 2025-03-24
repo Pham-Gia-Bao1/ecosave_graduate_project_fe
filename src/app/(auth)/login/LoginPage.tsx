09717ff1dcadaf63e40c28c08672b7a550c447e8
Bao Pham <bao.pham@sapotacorp.vn>
2025-03-24 16:31:05 +0700

    update redirect after login

--- src/app/(auth)/login/LoginPage.tsx
+++ src/app/(auth)/login/LoginPage.tsx
@@ -33,13 +33,14 @@ const Login = () => {
     e.preventDefault();
     setEmailError(!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email));
     setPasswordError(password.length < 6);
-
+    router.push("/")
     if (isFormValid) {
       setLoading(true);
       setErrorMessage(null); // Reset previous errors
       try {
         const data = await  api.auth.login(email, password);
         storeUserData(data);
+
       } catch (error: any) {
         const errorCode: keyof typeof loginErrors.errors =
           error?.response?.status || 500; // Explicitly type errorCode
